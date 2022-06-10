5022e2208e59406b36bc9eade00cf63ec85f6d75
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-06-10 19:10:54 -0300

    UI improvements

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ function App() {
       <header>
         <h3> Hi there. </h3>
         <h5> 
-          <p> Glad you found me. This is my portfolio and a React pet project. I'm likely working on it as you're reading this.</p>
-          <p> Big things are planned, you can check out <a href="https://github.com/alexphl/alexphl.github.io">the source code</a> in the meantime. </p>
+          <p> I'm Alex, glad you found me. This is my portfolio website and a React pet project. I'm likely working on it as you're reading this.</p>
+          <p> Big things are planned, you can check out the <a href="https://github.com/alexphl/alexphl.github.io">source code</a> in the meantime. </p>
         </h5> 
         
       </header>
@@ -23,13 +23,13 @@ function App() {
       <div id="projects">
         <h2>My other projects</h2>
         <div className='project'>
-          <h3>Barter - Android App</h3>
+          <h5>Barter - Android App</h5>
           <p>Java, Firebase</p>
           <a href="https://github.com/alexphl/csci3130-t7-online-barter-trading"><FiGithub className='icon'/></a>
         </div>
 
         <div className='project'>
-          <h3>Job Portal web app</h3>
+          <h5>Job Portal web app</h5>
           <p>JS, PHP, MySQL</p>
           <a href="https://web.cs.dal.ca/~olexiy/jobportal/"><HiOutlineExternalLink className='icon'/></a>
           <a href="https://github.com/alexphl/jobportal_IA"><FiGithub className='icon'/></a>
@@ -41,8 +41,8 @@ function App() {
       
       <footer>
         <div id='footerLinks'>
-          <a href="https://www.linkedin.com/in/alex-prokhvatylo-ba0471236/"><AiFillLinkedin/></a>
-          <a href="https://github.com/alexphl/"><AiFillGithub /></a>
+          <a href="https://www.linkedin.com/in/alex-prokhvatylo-ba0471236/"><AiFillLinkedin className='icon'/></a>
+          <a href="https://github.com/alexphl/"><AiFillGithub className='icon'/></a>
         </div>
       </footer>
     </>
