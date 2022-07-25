d61841b66d6abbd051fadd8a41b5ac3e908a2f2b
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-07-25 19:10:59 -0300

    Content updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { AiFillLinkedin, AiFillGithub } from "react-icons/ai";
-import Projects from "./components/Projects"
+import Projects from "./components/Projects";
+import Pdf from "./files/Resume.pdf";
 
 function App() {
   return (
@@ -9,14 +10,18 @@ function App() {
         <h5>
           <p>
             {" "}
-            I'm Alex, glad you found me. This is my portfolio website and a
-            React pet project. I'm likely working on it as you're reading this.
+            I'm Alex, glad you found me. I am a 4th year BCS student at
+            Dalhousie. My primary skillset that of web and mobile development
+            but I’m a generalist at heart. My main career interests lay in
+            full-stack web development, Cloud architecture and DevOps, though
+            I'm also passionate about mobile computing and UI/UX philosophies.
           </p>
           <p>
             {" "}
-            Big things are planned, you can check out the{" "}
-            <a href="https://github.com/alexphl/alexphl.github.io">
-              source code
+            This website is a work in progress, but you can check out my
+            old-fashioned{" "}
+            <a href={Pdf} target="_blank" rel="noreferrer">
+              PDF resume
             </a>{" "}
             in the meantime.{" "}
           </p>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -3,33 +3,49 @@ import { FiGithub } from "react-icons/fi";
 
 const projectList = [
 	{
-		id: 1,
+		title: "Personal website",
+		subHead: "ReactJS",
+		git: "https://github.com/alexphl/alexphl.github.io",
+		ext: null,
+		inProgress: true,
+	},
+	{
+		title: "Dalhousie Schedule Builder",
+		subHead: "Next.js, TypeScript",
+		git: "https://github.com/alexphl/alexphl.github.io",
+		ext: null,
+		inProgress: true,
+	},
+	{
 		title: "Barter - Android App",
 		subHead: "Java, Firebase",
 		git: "https://github.com/alexphl/csci3130-t7-online-barter-trading",
 		ext: null,
+		inProgress: false,
 	},
 	{
-		id: 2,
 		title: "Job Portal web app",
 		subHead: "JS, PHP, MySQL",
 		git: "https://github.com/alexphl/jobportal_IA",
 		ext: "https://web.cs.dal.ca/~olexiy/jobportal/",
+		inProgress: false,
 	},
 ];
 
 const Projects = (props) => {
 	return (
 		<div id="projects">
-			<h2>My other projects</h2>
+			<h2>My Projects</h2>
 
 			{projectList.map((project) => (
 				<div className="project">
 					<h5>{project.title}</h5>
 					<p>{project.subHead}</p>
-					{project.ext && <a href={project.ext}>
-						<HiOutlineExternalLink className="icon" />
-					</a>}
+					{project.ext && (
+						<a href={project.ext}>
+							<HiOutlineExternalLink className="icon" />
+						</a>
+					)}
 					<a href={project.git}>
 						<FiGithub className="icon" />
 					</a>
