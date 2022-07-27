b1f8efb235a24d34a93dc3b1e67736b4394b5b69
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-07-27 14:56:03 -0300

    Loading optimizations and style updates

--- pages/Head.tsx
+++ pages/Head.tsx
@@ -10,6 +10,12 @@ export const Heads = () => {
 				content="width=device-width, initial-scale=1"
 			/>
 			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.googleapis.com" /> 
+			<link rel="preconnect" href="https://fonts.gstatic.com" /> 
+			<link
+				href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=optional"
+				rel="stylesheet"
+			/>
 		</Head>
 	);
 };
