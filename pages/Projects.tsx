d0d8a7166e5c736fa462d137b6ece9c625788c22
Olexiy Prokhvatylo <ol797065@dal.ca>
2022-08-31 16:08:31 -0300

    Update projects

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -4,12 +4,21 @@ import Image from "next/image";
 
 const projectList = [
 	{
+		title: "Data visualizations for EQWorks",
+		subHead: "Next.js, tRPC, Prisma",
+		image: "/files/project_bg/EQWorks.png",
+		git: "https://github.com/alexphl/eqw-appdev-sol",
+		ext: "https://eqw-appdev-sol.vercel.app",
+		height: "180px",
+		label: "In Progress",
+	},
+	{
 		title: "Job Portal web app",
 		subHead: "JS, PHP, MySQL",
 		image: "/files/project_bg/bg_jobportal.png",
 		git: "https://github.com/alexphl/jobportal_IA",
 		ext: "https://web.cs.dal.ca/~olexiy/jobportal/",
-		height: '180px',
+		height: "180px",
 	},
 	{
 		title: "Barter - Android App",
@@ -17,13 +26,7 @@ const projectList = [
 		image: "/files/project_bg/barter.png",
 		git: "https://github.com/alexphl/csci3130-t7-online-barter-trading",
 		ext: null,
-		height: '180px',
-	},
-	{
-		title: "Dalhousie Schedule Builder",
-		subHead: "Next.js, TypeScript",
-		git: "https://github.com/alexphl/dal_schedule_builder",
-		ext: null,
+		height: "180px",
 	},
 	{
 		title: "Personal website (this one)",
@@ -42,10 +45,14 @@ const Projects = (props?: any) => (
 				className="project"
 				key={project.git}
 				style={{
-					backgroundImage: project.image && `url(${project.image})` || 'none',
-					height: project.height || 'auto',
+					backgroundImage:
+						(project.image && `url(${project.image})`) || "none",
+					height: project.height || "auto",
 				}}
 			>
+				{project.label && <div className="label" key={project.git}>
+									{project.label}
+								</div>}
 				<div className="projectDetails">
 					<h5>{project.title}</h5>
 					<p>{project.subHead}</p>
