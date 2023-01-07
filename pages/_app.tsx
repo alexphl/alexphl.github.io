30099532418392ae39f03a779cb523beb2574d83
Olexiy Prokhvatylo <ol797065@dal.ca>
2023-01-07 04:09:01 +0300

    Fix derp import

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-import { ParallaxProvider } from "react-scroll-parallax";
 
 export const config = {
 	unstable_runtimeJS: false,
